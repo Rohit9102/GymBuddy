7513ca867135f0d10cfdeddec6a61fdccc6e2bb6
Rohit9102 <155637241+Rohit9102@users.noreply.github.com>
2025-04-23 20:47:55 +0530

    Update api.js

--- frontend/api.js
+++ frontend/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000/auth'
+    baseURL: 'https://gymbuddy-backend-ylfz.onrender.com/auth'
 });
 
 export const googleAuth = (code) => api.get(`/google?code=${code}`);
